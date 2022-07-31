ef39231fb5acd2ba6bf369dfa1d3abea38d822b8
Mathieu Walravens <m.walravens@students.ephec.be>
2022-07-31 14:45:27 +0200

    Add Qr code component

---
+++ web/src/components/Qrcode.vue
@@ -0,0 +1,18 @@
+<template>
+  <canvas ref="canvas"></canvas>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref, watch } from "vue";
+import QRCode from "qrcode";
+
+const props = defineProps<{ value: string }>();
+const canvas = ref<HTMLCanvasElement>();
+
+const render = () => {
+  QRCode.toCanvas(canvas.value, props.value);
+};
+
+watch(props, render);
+onMounted(render);
+</script>
