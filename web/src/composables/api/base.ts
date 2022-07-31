376589485a1bc90ee91b4cd038491382e349f444
Mathieu Walravens <m.walravens@students.ephec.be>
2022-07-31 14:38:18 +0200

    Move files to /api

--- web/src/composables/api/base.ts
+++ web/src/composables/api/base.ts
@@ -1,7 +1,7 @@
 import { Ref } from "vue";
 
-export interface APIResponse<T> {
+export interface APIResponse<T, K = void> {
   data: Ref<T | undefined>;
   error: Ref<string | undefined>;
-  load: { (): Promise<void> };
+  load: { (data: K): Promise<void> };
 }
