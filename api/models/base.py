936cf53b04f43afdd3b8df77fd92c9bd78fd394e
Mathieu Walravens <m.walravens@students.ephec.be>
2022-08-03 02:34:41 +0200

    Add declarative base for sqlalchemy

--- api/models/base.py
+++ api/models/base.py
@@ -2,6 +2,8 @@ import os
 
 from databases import Database
 from sqlalchemy import MetaData
+from sqlalchemy.orm import declarative_base
 
 db = Database(os.environ["DATABASE_URL"])
 metadata = MetaData()
+Base = declarative_base(metadata=metadata)
