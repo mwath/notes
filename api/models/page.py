2f7f27f42fe1a48e054e5e1b7c098312956d504c
Mathieu Walravens <m.walravens@students.ephec.be>
2022-08-09 16:31:44 +0200

    Add blocks logic

--- api/models/page.py
+++ api/models/page.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from datetime import datetime
+import math
 
 from typing import Optional
 from typing_extensions import Self
@@ -86,4 +87,154 @@ class Page(BaseModel):
         return [cls(**u) for u in await db.fetch_all(query)]
 
 
+BlockId = constr(max_length=10)
+BlockType = constr(max_length=16)
 
+
+class BlockCreation(BaseModel):
+    type: BlockType
+    data: dict
+
+
+class BlockUpdate(BaseModel):
+    type: BlockType | None
+    data: dict | None
+
+
+class Block(BaseModel):
+    id: BlockId
+    page_id: int
+    type: BlockType
+    data: dict
+    sequence: int
+
+    @classmethod
+    async def get(cls, page_id: int, id_: BlockId) -> Block | None:
+        """Get a single block from the database."""
+        if block := await db.fetch_one(select(DBBlock).where(DBBlock.page_id == page_id, DBBlock.id == id_)):
+            return cls(**block)
+
+    @classmethod
+    async def delete(cls, page_id: int, block_id: BlockId) -> Block | None:
+        """Delete a single block from the database."""
+        async with db.transaction():
+            if block := await db.fetch_one(
+                delete(DBBlock).where(DBBlock.page_id == page_id, DBBlock.id == block_id).returning(DBBlock)
+            ):
+                block = cls(**block)
+                await db.execute(
+                    update(DBBlock)
+                    .values(sequence=DBBlock.sequence - 1)
+                    .where(DBBlock.page_id == page_id, DBBlock.sequence > block.sequence)
+                )
+                return block
+
+    @classmethod
+    async def get_slice(cls, page_id: int, from_: BlockId | None, size: int = 25) -> list[Block]:
+        """Return a range of blocks for a given page. To get the next range of blocks, call it with the last block's id."""
+        if from_ is None:
+            query = select(DBBlock).where(DBBlock.page_id == page_id)
+        else:
+            start = aliased(DBBlock)
+            query = (
+                select(DBBlock)
+                .join(start, start.page_id == DBBlock.page_id)
+                .where(
+                    start.id == from_,
+                    start.page_id == page_id,
+                    DBBlock.sequence > start.sequence,
+                )
+            )
+
+        return [cls(**b) for b in await db.fetch_all(query.order_by(DBBlock.sequence).limit(size))]
+
+    @classmethod
+    async def add(cls, page_id: int, block_id: BlockId, data: BlockCreation, sequence: int = None) -> Block:
+        """
+        Add a block to the page. Append it to the end if no sequence number is given.
+        Otherwise, it will insert the block in the page and increment the sequence of all subsequent blocks.
+        """
+        async with db.transaction():
+            if sequence is None:
+                one = literal_column("1")
+                sequence = select(func.count(one)).select_from(DBBlock).where(DBBlock.page_id == page_id).as_scalar()
+            else:
+                await db.execute(
+                    update(DBBlock)
+                    .values(sequence=DBBlock.sequence + 1)
+                    .where(DBBlock.page_id == page_id, DBBlock.sequence <= sequence)
+                )
+
+            return cls(
+                **await db.fetch_one(
+                    insert(DBBlock)
+                    .values(page_id=page_id, id=block_id, sequence=sequence, **data.dict())
+                    .returning(DBBlock)
+                )
+            )
+
+    @classmethod
+    async def update(cls, page_id: int, block_id: BlockId, data: BlockUpdate) -> Block | None:
+        """Update a block's type or data. To update the sequence, use `.swap` or `.move`."""
+        if block := await db.fetch_one(
+            update(DBBlock)
+            .values(**data.dict(exclude_none=True))
+            .where(DBBlock.id == block_id, DBBlock.page_id == page_id)
+            .returning(DBBlock)
+        ):
+            return cls(**block)
+
+    @classmethod
+    async def swap(cls, page_id: int, block1: BlockId, block2: BlockId):
+        """
+        Swap two blocks position.
+        Throws an error if one of the block does not exists.
+        The same error is thrown if the two blocks are identicals.
+        """
+        async with db.transaction():
+            rows = await db.fetch_all(
+                select(DBBlock.id, DBBlock.sequence).where(DBBlock.page_id == page_id, DBBlock.id in [block1, block2])
+            )
+            if len(rows) != 2:
+                missing = {block1, block2} - {row["id"] for row in rows}
+                raise ValueError(f"Unable to swap missing blocks: {missing}")
+
+            rows[0]["sequence"], rows[1]["sequence"] = rows[1]["sequence"], rows[0]["sequence"]
+            await db.execute_many(
+                update(DBBlock)
+                .values(sequence=bindparam("sequence"))
+                .where(DBBlock.page_id == page_id, DBBlock.id == bindparam("id")),
+                rows,
+            )
+
+    @classmethod
+    async def move(cls, page_id: int, block_id: BlockId, dst: int):
+        """Move a block's position to anywhere. Will update sequence of other blocks."""
+        async with db.transaction():
+            src = await db.fetch_val(
+                select(DBBlock.sequence).where(
+                    DBBlock.id == block_id,
+                    DBBlock.page_id == page_id,
+                )
+            )
+            if dst == src:
+                return
+
+            direction = (src - dst) // abs(src - dst)
+            await db.execute(
+                update(DBBlock)
+                .values(sequence=DBBlock.sequence + direction)
+                .where(
+                    DBBlock.page_id == page_id,
+                    src < DBBlock.sequence if direction < 0 else dst <= DBBlock.sequence,
+                    src > DBBlock.sequence if direction > 0 else dst >= DBBlock.sequence,
+                )
+            )
+            await db.execute(
+                update(DBBlock)
+                .values(sequence=dst)
+                .where(
+                    DBBlock.page_id == page_id,
+                    DBBlock.id == block_id,
+                )
+            )
