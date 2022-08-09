34ac7d0e13c89b32e3e97e4da8e7825819bb6af1
Mathieu Walravens <m.walravens@students.ephec.be>
2022-08-09 16:13:02 +0200

    Add packages
    Add toasts, sass and moment.js

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,8 +3,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import Toast, { PluginOptions } from "vue-toastification";
 
 import router from "./router";
+
+import "vue-toastification/dist/index.css";
 import "./style.css";
 
 loadFonts();
@@ -12,4 +15,4 @@ loadFonts();
 const app = createApp(App);
 const pinia = createPinia();
 
-app.use(vuetify).use(router).use(pinia).mount("#app");
+app.use(vuetify).use(router).use(pinia).use(Toast).mount("#app");
