87fcf94cfc428195f118fcee87aecceed9182bda
Mathieu Walravens <m.walravens@students.ephec.be>
2022-09-01 08:19:16 +0200

    Improve settings page

--- web/src/views/Settings.vue
+++ web/src/views/Settings.vue
@@ -1,128 +1,222 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col>
-        Thème
-        <v-btn-toggle
-          v-model="themeStore.theme"
-          mandatory
-          divided
-          color="primary"
-        >
-          <v-btn value="dark" prepend-icon="mdi-moon-waning-crescent">
-            Sombre
-          </v-btn>
-          <v-btn value="system" prepend-icon="mdi-theme-light-dark">
-            Système
-          </v-btn>
-          <v-btn value="light" prepend-icon="mdi-white-balance-sunny">
-            Clair
-          </v-btn>
-        </v-btn-toggle>
-      </v-col>
+    <v-row justify="center">
+      <v-divider />
+      <h2>Thème</h2>
+      <v-divider />
     </v-row>
-    <v-row>
-      <v-col>
-        <v-btn
-          color="primary"
-          :disabled="userStore.user?.has2fa"
-          @click="openModal('enable')"
-        >
-          Enable 2FA
+    <v-row class="my-6" justify="center">
+      <v-btn-toggle v-model="$theme.theme" mandatory divided color="primary">
+        <v-btn value="dark" prepend-icon="mdi-moon-waning-crescent">
+          Sombre
         </v-btn>
-        <v-btn
-          color="primary"
-          :disabled="!userStore.user?.has2fa"
-          @click="openModal('disable')"
-        >
-          Disable 2FA
+        <v-btn value="system" prepend-icon="mdi-theme-light-dark">
+          Système
         </v-btn>
-        <v-dialog v-model="dialog2fa">
-          <v-card>
-            <v-card-title class="text-center text-capitalize"
-              >{{ dialogAction }} 2FA</v-card-title
-            >
-            <v-card-text>
-              <v-alert
-                v-if="new2faError || enable2faError || disable2faError"
-                type="error"
-                variant="outlined"
-                icon="mdi-cloud-alert"
-                :text="new2faError || enable2faError || disable2faError"
-              />
-              <v-container v-if="dialogAction == 'enable'">
-                <v-row justify="center" no-gutters>
-                  Scan the image below with the two-factor authentification app
-                  on your phone.
-                  <a href="#" @click.prevent="showURI = !showURI">
-                    If you can't use a QR code, enter this text code instead
-                  </a>
-                  .
-                </v-row>
-                <v-row v-if="showURI" justify="center" no-gutters>
-                  {{ new2fa?.uri }}
-                </v-row>
-                <v-row justify="center" class="ma-6" no-gutters>
-                  <qrcode v-if="new2fa" :value="new2fa.uri" />
-                  <v-progress-circular v-else indeterminate />
-                </v-row>
-                <v-row justify="center" no-gutters>
-                  <div class="font-weight-bold">
-                    Enter the six-digit code from the application.
-                  </div>
-                  <div>
-                    After scanning the QR code image, the app will display a
-                    six-digit code that you can enter below.
-                  </div>
-                </v-row>
-              </v-container>
-              <v-container v-else>
-                <v-row justify="center" no-gutters>
-                  To disable the two-factor authentification, enter the
-                  six-digit code from the 2FA application on your phone.
-                </v-row>
-              </v-container>
-              <v-otp-input
-                ref="otpInput"
-                class="justify-center"
-                input-classes="otp-input"
-                input-type="tel"
-                separator=""
-                :num-inputs="6"
-                should-auto-focus
-                @on-complete=""
-              />
-            </v-card-text>
-            <v-card-actions>
-              <v-row justify="space-around">
+        <v-btn value="light" prepend-icon="mdi-white-balance-sunny">
+          Clair
+        </v-btn>
+      </v-btn-toggle>
+    </v-row>
+    <v-row class="mt-16 mb-4" justify="center">
+      <v-divider />
+      <h2>Paramètres du compte</h2>
+      <v-divider />
+    </v-row>
+    <v-row justify="center">
+      <h3>Double authentification</h3>
+    </v-row>
+    <v-row class="my-6" justify="center">
+      <v-btn
+        color="primary"
+        @click="openModal($user.user?.has2fa ? 'disable' : 'enable')"
+      >
+        {{ $user.user?.has2fa ? "Désactiver" : "Activer" }}
+      </v-btn>
+      <v-dialog v-model="dialog2fa">
+        <v-card>
+          <v-card-title class="text-center text-capitalize"
+            >{{ dialogAction }} 2FA</v-card-title
+          >
+          <v-card-text>
+            <v-alert
+              v-if="new2faError || enable2faError || disable2faError"
+              type="error"
+              variant="outlined"
+              icon="mdi-cloud-alert"
+              :text="new2faError || enable2faError || disable2faError"
+            />
+            <v-container v-if="dialogAction == 'enable'">
+              <v-row justify="center" no-gutters>
+                Scan the image below with the two-factor authentification app on
+                your phone.
+                <a href="#" @click.prevent="showURI = !showURI">
+                  If you can't use a QR code, enter this text code instead
+                </a>
+                .
+              </v-row>
+              <v-row v-if="showURI" justify="center" no-gutters>
+                {{ new2fa?.uri }}
+              </v-row>
+              <v-row justify="center" class="ma-6" no-gutters>
+                <qrcode v-if="new2fa" :value="new2fa.uri" />
+                <v-progress-circular v-else indeterminate />
+              </v-row>
+              <v-row justify="center" no-gutters>
+                <div class="font-weight-bold">
+                  Enter the six-digit code from the application.
+                </div>
+                <div>
+                  After scanning the QR code image, the app will display a
+                  six-digit code that you can enter below.
+                </div>
+              </v-row>
+            </v-container>
+            <v-container v-else>
+              <v-row justify="center" no-gutters>
+                To disable the two-factor authentification, enter the six-digit
+                code from the 2FA application on your phone.
+              </v-row>
+            </v-container>
+            <v-otp-input
+              ref="otpInput"
+              class="justify-center"
+              input-classes="otp-input"
+              input-type="tel"
+              separator=""
+              :num-inputs="6"
+              should-auto-focus
+              @on-complete=""
+            />
+          </v-card-text>
+          <v-card-actions>
+            <v-row justify="space-around">
+              <v-btn
+                :color="dialogAction == 'enable' ? 'error' : 'success'"
+                @click="dialog2fa = false"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                :color="dialogAction == 'enable' ? 'success' : 'error'"
+                @click="dialogAction == 'enable' ? enable2FA() : disable2FA()"
+              >
+                {{ dialogAction }}
+              </v-btn>
+            </v-row>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
+    <v-row justify="center">
+      <h3>Déconnexion</h3>
+    </v-row>
+    <v-row justify="center" class="my-6">
+      <v-btn
+        color="red-lighten-1"
+        variant="plain"
+        append-icon="mdi-exit-to-app"
+        @click="logout()"
+      >
+        Se déconnecter
+      </v-btn>
+    </v-row>
+    <v-row justify="center">
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-title class="text-center">
+            <h2>Plus d'actions</h2>
+          </v-expansion-panel-title>
+          <v-expansion-panel-text>
+            <v-container>
+              <v-row justify="center">
+                <h3>Suppression du compte</h3>
+              </v-row>
+              <v-row justify="center" class="my-6">
+                <v-alert type="error" variant="outlined" icon="mdi-alert">
+                  Attention, cette action est irreversible ! Le compte sera
+                  perdu à tout jamais et il sera impossible de le récupérer.
+                  <i>Note: Vos pages ne seront pas supprimées.</i>
+                </v-alert>
+              </v-row>
+              <v-row justify="center" class="my-6">
                 <v-btn
-                  :color="dialogAction == 'enable' ? 'error' : 'success'"
-                  @click="dialog2fa = false"
+                  color="red-lighten-1"
+                  prepend-icon="mdi-trash-can"
+                  @click="dialogDelete = true"
                 >
-                  Cancel
-                </v-btn>
-                <v-btn
-                  :color="dialogAction == 'enable' ? 'success' : 'error'"
-                  @click="dialogAction == 'enable' ? enable2FA() : disable2FA()"
-                >
-                  {{ dialogAction }}
+                  Supprimer le compte
                 </v-btn>
               </v-row>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-btn
-          color="red-lighten-1"
-          append-icon="mdi-exit-to-app"
-          @click="logout()"
-        >
-          Se déconnecter
-        </v-btn>
-      </v-col>
+            </v-container>
+            <v-dialog v-model="dialogDelete">
+              <v-card>
+                <v-card-title class="text-center">
+                  Suppression du compte
+                </v-card-title>
+                <v-card-text>
+                  <v-alert
+                    v-if="deleteError"
+                    type="error"
+                    variant="outlined"
+                    icon="mdi-cloud-alert"
+                    :text="deleteError"
+                  />
+                  <v-container>
+                    <v-row justify="center" no-gutters>
+                      Pour supprimer votre compte, veuillez récrire ci-dessous,
+                      votre nom d'utilisateur: <b>{{ $user.user?.username }}</b
+                      >.
+                    </v-row>
+                    <v-row class="my-6">
+                      <v-text-field
+                        v-model="username"
+                        label="Nom d'utilisateur"
+                        required
+                        hide-details="auto"
+                      />
+                    </v-row>
+                  </v-container>
+                  <v-container v-if="$user.user?.has2fa">
+                    <v-row class="mb-2">
+                      Une confirmation à l'aide de votre code de double
+                      authentification est requise pour la suppression de votre
+                      compte.
+                    </v-row>
+                    <v-otp-input
+                      :is-disabled="true"
+                      ref="otpInput"
+                      class="justify-center"
+                      input-classes="otp-input"
+                      input-type="tel"
+                      separator=""
+                      :num-inputs="6"
+                      @on-complete=""
+                    />
+                  </v-container>
+                </v-card-text>
+                <v-card-actions>
+                  <v-row justify="space-around">
+                    <v-btn color="success" @click="dialogDelete = false">
+                      Annuler
+                    </v-btn>
+                    <v-btn
+                      color="error"
+                      :disabled="
+                        username != $user.user?.username ||
+                        (!!otpInput && otpInput?.otp.join('')?.length != 6)
+                      "
+                      @click="deleteAccount()"
+                    >
+                      Supprimer
+                    </v-btn>
+                  </v-row>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </v-row>
   </v-container>
 </template>
@@ -139,13 +233,17 @@ import {
   disable2FA as disable2FAInit,
 } from "../composables/api/auth/2fa";
 import { useUserStore } from "$/user";
+import requests from "@/composables/api/requests";
 
-const dialog2fa = ref(false);
 const showURI = ref(false);
+const dialog2fa = ref(false);
+const dialogDelete = ref(false);
+const deleteError = ref<string>();
+const username = ref<string>("");
 const dialogAction = ref<"enable" | "disable">();
 const otpInput = ref<{ otp: string[] } | null>(null);
-const themeStore = useThemeStore();
-const userStore = useUserStore();
+const $theme = useThemeStore();
+const $user = useUserStore();
 const { load: logout } = logoutInit();
 
 const { data: new2fa, error: new2faError, load: request2FA } = request2FAInit();
@@ -159,7 +257,7 @@ async function enable2FA() {
 
     if (!enable2faError.value) {
       dialog2fa.value = false;
-      userStore.reload();
+      $user.reload();
     }
   } else {
     enable2faError.value = "Code invalide";
@@ -173,10 +271,29 @@ async function disable2FA() {
 
     if (!disable2faError.value) {
       dialog2fa.value = false;
-      userStore.reload();
+      $user.reload();
     }
   } else {
     disable2faError.value = "Code invalide";
+  }
+}
+
+async function deleteAccount() {
+  let otp = undefined;
+  if ($user.user?.has2fa) {
+    const otp = otpInput.value?.otp.join("");
+    if (otp?.length != 6) {
+      deleteError.value = "Code invalide";
+      return;
+    }
+  }
+
+  try {
+    await requests.delete("/users/me", { data: { code: otp } });
+    dialogDelete.value = false;
+    $user.reload();
+  } catch (err: any) {
+    deleteError.value = err?.response?.data?.detail || err.message;
   }
 }
 
