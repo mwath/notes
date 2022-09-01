c243a8aa64f66f4dd335b6c85d00ec4b684ccc7a
Mathieu Walravens <m.walravens@students.ephec.be>
2022-09-01 08:19:03 +0200

    Add setup.sh
    Script that helps setup the database.

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+docker-compose exec --user=$UID --workdir=/app/api api alembic upgrade head
+docker-compose exec db psql postgres postgres -c "INSERT INTO users(id, username, email, password) VALUES(0, 'DeletedAccount', 'deleted', '')"
